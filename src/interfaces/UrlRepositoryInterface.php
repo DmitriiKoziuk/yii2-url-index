6cb79aae4d1926acb2d593888c9a5a3bfb686dcd
Dmitrii Koziuk <dmitrii.koziuk@gmail.com>
2019-09-19 19:35:28 +0300

    UrlRepositoryInterface extends ActiveRecordRepositoryInterface

--- src/interfaces/UrlRepositoryInterface.php
+++ src/interfaces/UrlRepositoryInterface.php
@@ -2,9 +2,10 @@
 
 namespace DmitriiKoziuk\yii2UrlIndex\interfaces;
 
+use DmitriiKoziuk\yii2Base\interfaces\ActiveRecordRepositoryInterface;
 use DmitriiKoziuk\yii2UrlIndex\entities\UrlEntity;
 
-interface UrlRepositoryInterface
+interface UrlRepositoryInterface extends ActiveRecordRepositoryInterface
 {
     public function getById(int $id): ?UrlEntity;
 
