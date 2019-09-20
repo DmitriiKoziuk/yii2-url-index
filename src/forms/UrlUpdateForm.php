9eacd053cd498e47ef5d5a06bee9c7f73a51953c
Dmitrii Koziuk <dmitrii.koziuk@gmail.com>
2019-09-20 14:08:43 +0300

    Add property: id, created_at, updated_at.

--- src/forms/UrlUpdateForm.php
+++ src/forms/UrlUpdateForm.php
@@ -2,6 +2,23 @@
 
 namespace DmitriiKoziuk\yii2UrlIndex\forms;
 
+use yii\helpers\ArrayHelper;
+
 class UrlUpdateForm extends UrlCreateForm
 {
+    public $id;
+
+    public $created_at;
+
+    public $updated_at;
+
+    public function rules()
+    {
+        return ArrayHelper::merge(
+            parent::rules(),
+            [
+                ['id', 'created_at', 'updated_at'], 'integer'
+            ]
+        );
+    }
 }
