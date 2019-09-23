230642053fc044070d3351de4f18f638f395ab8c
Dmitrii Koziuk <dmitrii.koziuk@gmail.com>
2019-09-23 14:01:56 +0300

    Write tests for url update page.

---
+++ tests/acceptance/UrlUpdatePageCest.php
@@ -0,0 +1,105 @@
+<?php declare(strict_types=1);
+
+namespace DmitriiKoziuk\yii2UrlIndex\tests;
+
+use Codeception\Example;
+use DmitriiKoziuk\yii2UrlIndex\tests\_fixtures\UrlsFixture;
+use DmitriiKoziuk\yii2UrlIndex\tests\_fixtures\UserFixture;
+
+class UrlUpdatePageCest
+{
+    public function _fixtures()
+    {
+        return [
+            'user' => [
+                'class' => UserFixture::class,
+                'dataFile' => codecept_data_dir() . 'login_data.php'
+            ],
+            'urls' => [
+                'class' => UrlsFixture::class,
+                'dataFile' => codecept_data_dir() . 'url_data.php'
+            ]
+        ];
+    }
+
+    public function trySignIn(AcceptanceTester $I)
+    {
+        $I->amOnPage('/site/login');
+        $I->see('Please fill out the following fields to login:');
+        $I->fillField('LoginForm[username]', 'erau');
+        $I->fillField('LoginForm[password]', 'password_0');
+        $I->click('Login', '#login-form');
+        $I->seeResponseCodeIs(200);
+        $I->see('Logout (erau)');
+        $I->dontSeeLink('Login');
+        $I->dontSeeLink('Signup');
+    }
+
+    /**
+     * @param AcceptanceTester $I
+     * @param Example $existUrls
+     * @depends trySignIn
+     * @dataProvider urlDataProvider
+     */
+    public function existsOnPage(AcceptanceTester $I, Example $existUrls)
+    {
+        $I->wantTo('Check is all field exist on page.');
+        $I->amOnPage("/dk-url-index/url/update?id={$existUrls['id']}");
+        $I->seeResponseCodeIs(200);
+        $I->see("Update Url Index Entity: {$existUrls['id']}", 'h1');
+
+        $I->seeElement('input', ['name' => "UrlEntity[url]"]);
+        $I->seeElement('input', ['name' => 'UrlEntity[redirect_to_url]']);
+        $I->seeElement('input', ['name' => 'UrlEntity[module_name]']);
+        $I->seeElement('input', ['name' => 'UrlEntity[controller_name]']);
+        $I->seeElement('input', ['name' => 'UrlEntity[action_name]']);
+        $I->seeElement('input', ['name' => 'UrlEntity[action_name]']);
+    }
+
+    /**
+     * @param AcceptanceTester $I
+     * @param Example $existUrls
+     * @depends trySignIn
+     * @dataProvider urlDataProvider
+     */
+    public function hasRelevantData(AcceptanceTester $I, Example $existUrls)
+    {
+        $I->wantTo('Check is all field has relevant data.');
+        $I->amOnPage("/dk-url-index/url/update?id={$existUrls['id']}");
+        $I->seeResponseCodeIs(200);
+        $I->see("Update Url Index Entity: {$existUrls['id']}", 'h1');
+
+        $I->seeInField(['name' => "UrlEntity[url]"], $existUrls['url']);
+        $value = $existUrls['redirect_to_url'] ?? '';
+        $I->seeInField(['name' => 'UrlEntity[redirect_to_url]'], $value);
+        $value = $existUrls['module_name'] ?? '';
+        $I->seeInField(['name' => 'UrlEntity[module_name]'], $value);
+        $I->seeInField(['name' => 'UrlEntity[controller_name]'], $existUrls['controller_name']);
+        $I->seeInField(['name' => 'UrlEntity[action_name]'], $existUrls['action_name']);
+        $I->seeInField(['name' => 'UrlEntity[action_name]'], $existUrls['action_name']);
+    }
+
+    /**
+     * @param AcceptanceTester $I
+     * @param Example $existUrls
+     * @depends trySignIn
+     * @dataProvider urlDataProvider
+     */
+    public function hasSaveButton(AcceptanceTester $I, Example $existUrls)
+    {
+        $I->wantTo('Check is page has "Save" button');
+        $I->amOnPage("/dk-url-index/url/update?id={$existUrls['id']}");
+        $I->seeResponseCodeIs(200);
+        $I->see("Update Url Index Entity: {$existUrls['id']}", 'h1');
+
+        $I->seeElement('button#save-url[type="submit"]');
+    }
+
+    /**
+     * @return array
+     */
+    protected function urlDataProvider()
+    {
+        return include codecept_data_dir() . 'url_data.php';
+    }
+}
