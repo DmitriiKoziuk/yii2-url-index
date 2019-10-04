4ec575bc2ed1837ed3109dfb891cc8a45e46a4aa
Dmitrii Koziuk <dmitrii.koziuk@gmail.com>
2019-10-04 16:06:03 +0300

    Fix tests. Do not run my modules because yii vendor path link to wrong place.

--- tests/_app/config/backend-test.php
+++ tests/_app/config/backend-test.php
@@ -1,6 +1,7 @@
 <?php
 
 return yii\helpers\ArrayHelper::merge(
+    require __DIR__ . '/../../../../../../common/config/main.php',
     require __DIR__ . '/../../../../../../common/config/main-local.php',
     require __DIR__ . '/../../../../../../common/config/test-local.php',
     require __DIR__ . '/../../../../../../backend/config/main.php',
