9a999804b36ea5b1219ea1e614cafdcb33571b19
Dmitrii Koziuk <dmitriikoziuk@gmail.com>
2019-10-09 15:05:07 +0300

    Create UpdateEntityUrlFormNotValidException

---
+++ src/exceptions/UpdateEntityUrlFormNotValidException.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace DmitriiKoziuk\yii2UrlIndex\exceptions;
+
+use DmitriiKoziuk\yii2Base\exceptions\DataNotValidException;
+
+class UpdateEntityUrlFormNotValidException extends DataNotValidException
+{
+}
